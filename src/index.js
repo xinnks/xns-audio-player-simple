81132e8e9f2ee4de30110c23d64eaae949569981
James Sinkala <xinnks@gmail.com>
2020-10-25 04:52:25 +0300

    improvement(src): replace xns-seek-bar plugin with a custom progress-bar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import XnsAudioPlayerSimple from "./XnsAudioPlayerSimple.vue";
-import XnsSeekBar from 'xns-seek-bar'
 
 export default {
-    install(Vue, options = { progressBar: XnsSeekBar}) {
+    install(Vue) {
         // register component globally
         // https://vuejs.org/v2/guide/components-registration.html
-        Vue.use(options.progressBar)
         Vue.component("xns-audio-player-simple", XnsAudioPlayerSimple);
     }
 }
