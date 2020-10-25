a6c46dcf301559ad4fd807f675280457d60f0761
James Sinkala <xinnks@gmail.com>
2020-10-25 06:51:00 +0300

    refactor: update rollup and post-css configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,15 @@
 import nodeResolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
+import commonjs from "@rollup/plugin-commonjs";
 import postcss from "rollup-plugin-postcss";
-import commonjs from 'rollup-plugin-commonjs'; // Convert CommonJS modules to ES6
+import commonjs from '@rollup/plugin-commonjs'; // Convert CommonJS modules to ES6
 import vue from 'rollup-plugin-vue'; // Handle .vue SFC files
-import buble from 'rollup-plugin-buble'; // Transpile/polyfill with reasonable browser support
+import buble from '@rollup/plugin-buble'; // Transpile/polyfill with reasonable browser support
 
 export default {
     input: 'src/index.js', // Path relative to package.json
     output: {
         name: 'XnsAudioPlayerSimple',
-    	file: 'dist/index.js',
+    	  file: 'dist/index.js',
         exports: 'named',
         format: 'cjs'
     },
