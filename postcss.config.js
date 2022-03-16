561861dba9184da3c8640be64bae3d26381f0cc6
James Sinkala <xinnks@gmail.com>
2022-03-16 23:29:08 +0300

    improvement: Update reconfigure tailwindcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,6 @@
 module.exports = {
-    plugins: [
-        require("postcss-preset-env"),
-        require("tailwindcss"),
-        require("@fullhuman/postcss-purgecss")({
-
-            // Specify the paths to all of the template files in your project
-            content: [
-                "./src/XnsAudioPlayerSimple.vue"
-            ],
-        })
-    ],
-};
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    }
+}
