ad82eac82c409b0120d3f0cf58ce761e929b719e
James Sinkala <xinnks@gmail.com>
2022-03-18 10:06:06 +0300

    feat: change component name

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import XnsAudioPlayerSimple from "./XnsAudioPlayerSimple.vue";
 
 export default {
     install: (app, options) => {
-        // register component globally
-        // https://vuejs.org/v2/guide/components-registration.html
-        app.component("xns-audio-player-simple", XnsAudioPlayerSimple);
+        app.component("XnsAudioPlayerSimple", XnsAudioPlayerSimple);
     }
 }
